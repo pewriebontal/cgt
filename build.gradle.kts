6c9da8095c089c74826f6ca7065038b931bb8a98
Pewrie Bontal <0x@bontal.net>
2024-02-07 20:45:47 +0800

    added helper scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "net.bontal"
@@ -14,6 +15,20 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+application {
+    mainClass = "net.bontal.cgt.CgtInterface" 
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+    args("--console=plain")
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("generateJavadoc", Javadoc::class) {
+    source = fileTree("src/main/java/net/bontal/cgt")
+    destinationDir = file("doc")
 }
